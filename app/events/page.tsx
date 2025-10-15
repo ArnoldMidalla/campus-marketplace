52730a7e00682b415643feecbd6c13f2cd072d82
Arnold Midalla <midallaarnold@gmail.com>
2025-10-15 09:41:57 +0100

    undid blur events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,8 +3,9 @@ import FilteredEventItems from "../my_components/filteredEventItems";
 export default function Events() {
   return (
     <main className="min-h-screen pt-20 flex justify-center">
-      <p className="fixed text-center  translate-y-1/2 font-extrabold text-4xl z-10 tracking-tight">Coming soon</p>
-      <div className="max-w-5xl flex flex-col blur-lg">
+      {/* <p className="fixed text-center  translate-y-1/2 font-extrabold text-4xl z-10 tracking-tight">Coming soon</p> */}
+      <div className="max-w-5xl flex flex-col">
+        {/* blur-lg */}
         <h1 className="text-3xl font-bold text-center tracking-tight">
           Events around you
         </h1>
